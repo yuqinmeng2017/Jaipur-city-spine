acfecb9ac8033e681fcb6532529ef6fe8154f0cd
yuqinmeng2017 <56670899+yuqinmeng2017@users.noreply.github.com>
2019-11-13 21:36:17 -0500

    add new maps
    new map

---
+++ map.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoieXVxaW5tZW5nMjAxNyIsImEiOiJjamc4ZnBpdjcxMDRmMnhxbDN0bHRiamNpIn0.8dW5tp4Ek1dn7GJvSchfqg'; 
+var map = new mapboxgl.Map({
+	container: 'map',   
+	style: 'mapbox://styles/yuqinmeng2017/ck1xxatee24861cn0sjbktk2d',
+});
